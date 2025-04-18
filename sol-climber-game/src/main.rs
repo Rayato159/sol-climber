2d23ef37324a4711c7ad2c87f95f3fd59c6cc897
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2025-04-19 00:20:51 +0700

    :sparkles: fix camera zoom

--- sol-climber-game/src/main.rs
+++ sol-climber-game/src/main.rs
@@ -4,7 +4,7 @@ use sol_climber::{components, resources::camera_setting::CameraSetting, systems}
 fn main() {
     let camera_settings = CameraSetting {
         viewport_height: 5.,
-        zoom_range: 1.0..2.0,
+        zoom_range: 0.5..2.0,
         zoom_speed: 0.1,
     };
 
@@ -14,7 +14,10 @@ fn main() {
         components::light::spawn_light,
     );
 
-    let camera_system_bundle = (systems::core::camera_movement, systems::core::camera_zoom);
+    let camera_system_bundle = (
+        systems::camera::camera_movement,
+        systems::camera::camera_zoom,
+    );
 
     App::new()
         .insert_resource(camera_settings)
